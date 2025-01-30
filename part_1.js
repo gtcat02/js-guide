1f3e52c685d747e396f316bc4f500b66edba714b
Alexandr <gtcat02@gmail.com>
2025-01-30 19:03:17 +0400

    Сделал первую часть заметок

---
+++ part_1.js
@@ -0,0 +1,39 @@
+let num = 42; // number
+let firstName = 'Alexandr'; // string
+const isProgrammer = true; // boolean
+
+// isProgrammer = false; error
+
+/* Can Do
+let $hello = 'test';
+let _ = 49;
+let _num = 11;
+let myNum = false;
+*/
+
+/* Restricted
+let 42num = 1;
+let my-fame = 0;
+let let = 1;
+*/
+
+// firstName = 'Alex';
+// alert(firstName);
+// console.log('Test:', firstName);
+
+// console.log(num);
+// console.log(num + 5);
+// console.log(num - 5);
+// console.log(num * 5);
+// console.log(num / 5);
+// console.log(num);
+
+// num += 8;
+// console.log(num);
+
+// let num2 = (num + 43) + 1 - 3 * 3;
+// console.log(num2);
+
+// const fullName = firstName + ' ' + 'Andrienko';
+// console.log(fullName);
+
